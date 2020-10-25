209f08e5ba315ed29c7d3b475751f5877b66f688
Jayjay <jayjayish@gmail.com>
2020-10-25 15:59:12 -0600

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,30 +2,92 @@ import express from 'express'
 import bodyparser from 'body-parser'
 import { admin } from './config/firebase-config'
 
-const app = express()
-const mongo = require('mongodb')
 
-app.use(bodyparser.json())
+const express = require('express');
+const app = express();
+app.use(bodyparser.json());
+
+
+const mongoClient = require('mongodb').MongoClient;
+const mongoURL = 'mongodb://localhost:27017';
+
+var db;
+var dbTokens;
 
 const port = 3000
 const notification_options = {
     priority: "high",
     timeToLive: 60 * 60 * 24 // a days worth of seconds
   };
-const MongoClient = mongo.MongoClient
-const mongo_url = ''
+
+mongoClient.connect(mongoURL, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+    if (err) return console.log(err);
+
+    db = client.db('users');
+    app.listen(3000, function() {
+        console.log('Database server exists');
+    })
+
+})
+
+
+app.post('/users', (req, res) => {
+    db.collection("users").insertOne({"username":req.body.username, "realName":req.body.realName}, (err, result) => {
+        if (req.body.username == null || req.body.realName == null){
+            res.status(400).send("error, username not passed or real name not passed");
+            return;
+        }
+        if (err) return console.log(err);
+        res.send("saved\n");
+    })
+})
+
+app.put('/users', (req, res) => {
+    db.collection("users").updateOne({"username":req.body.username}, {$set:{"info":req.body.realName}}, (err, result) => {
+        if (req.body.username == null || req.body.realName == null){
+            res.status(400).send("error, username not passed or real name not passed");
+            return;
+        }
+        if (err) return console.log(err);
+        res.send("updated\n");
+    })
+
+})
+
+
+app.get('/users', (req, res) => {
+    db.collection("users").find().toArray((err, result) => {
+        res.send(result);
+    })
+})
+
+app.delete('/users', (req, res) => {
+    db.collection("users").deleteOne({"realName": req.body.realName}, (err, result) =>{
+        if (req.body.username == null || req.body.realName == null){
+            res.status(400).send("error, username not passed or real name not passed");
+            return;
+        }
+        if(err) return console.log(err);
+        res.send("removed\n");
+    })
+})
+
+
+
+
 
 app.post('/registerToken',(req, res)=>{
     // TODO: send to database
-    MongoClient.connect(url, (err, db) => {
-        if (err) throw err
-        else {
-            db.collection('tokens').insertOne(req.body, (err, body) => {
-                if (err) throw err
-                res.sendStatus(200)
-            })
-        }
-        db.close()
+    mongoClient.connect(mongoURL, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+        if (err) return console.log(err);
+
+        dbTokens = client.db('tokens');
+        dbTokens.collection('tokens').insertOne(req.body, (err, result) => {
+            if (err) throw console.log(err);
+            res.sendStatus(200);
+        })
+        
+        dbTokens.close();
     })
 });
 
