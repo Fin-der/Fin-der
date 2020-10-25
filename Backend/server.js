1b3f91987beedac0fdfab9c3a4a7e200cc8ba19d
cody li <licody2000@gmail.com>
2020-10-24 17:02:16 -0700

    add part of FCM to backend

---
+++ Backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import bodyparser from 'body-parser'
+import { admin } from './config/firebase-config'
+
+const app = express()
+app.use(bodyparser.json())
+
+const port = 3000
+const notification_options = {
+    priority: "high",
+    timeToLive: 60 * 60 * 24 // a days worth of seconds
+  };
+
+app.post('/firebase/notification', (req, res)=>{
+    const registrationToken = req.body.registrationToken
+    const message = req.body.message
+    const options = notification_options
+    
+    admin.messaging().sendToDevice(registrationToken, message, options)
+    .then( response => {
+
+    res.status(200).send("Notification sent successfully")
+    
+    })
+    .catch( error => {
+        console.log(error);
+    });
+
+})
+app.listen(port, () =>{
+    console.log("listening to port"+port)
+})
