fe275d68bbd1dec4dcf607c8858ad8f094282140
cody li <licody2000@gmail.com>
2020-10-26 11:01:10 -0700

    FCM

--- Backend/routes/chatRoom.js
+++ Backend/routes/chatRoom.js
@@ -5,7 +5,10 @@ import chatRoom from '../controllers/chatRoom.js';
 const router = express.Router();
 
 router
+    // @params: user id
+    // @returns: chatroom info
     .get('/', chatRoom.getRecentConversation)
+    // @params: room id
     .get('/:roomId', chatRoom.getConversationByRoomId)
     .post('/initiate', chatRoom.initiate)
     .post('/:roomId/message', chatRoom.postMessage)
