e6268a6f57f03755a070165fce7e81e7fd626b78
jack5661 <34701860+jack5661@users.noreply.github.com>
2020-10-31 23:35:02 -0700

    Added layout for matchView

---
+++ Frontend/app/src/main/java/com/example/finder/Views/MatchView.java
@@ -0,0 +1,16 @@
+package com.example.finder.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finder.R;
+
+public class MatchView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_view2);
+    }
+}
