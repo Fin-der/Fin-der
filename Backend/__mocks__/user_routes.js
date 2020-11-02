14d920d1279b12d45641c8e9dae64a2eb2c87f3f
Jayjay <jayjayish@gmail.com>
2020-11-01 18:18:49 -0700

    server endpoint tests kind of work

---
+++ Backend/__mocks__/user_routes.js
@@ -0,0 +1,13 @@
+const user = {
+    4: {name: 'Mark'},
+    5: {name: 'Paul'},
+  };
+  
+  export default function request(url) {
+    return new Promise((resolve, reject) => {
+      const userID = parseInt(url.substr('/users/'.length), 10);
+      process.nextTick(() =>
+        resolve(user)
+      );
+    });
+  }
--- Backend/server.js
+++ Backend/server.js
@@ -1,23 +1,23 @@
 import express from 'express'
-import bodyparser from 'body-parser'
+//import bodyparser from 'body-parser'
 import admin from './config/firebase-config.js'
-import cors from 'cors'
+//import cors from 'cors'
 import socketio from 'socket.io'
 import http from 'http'
 import "./config/mongo.js"
 // socket configuration
 import WebSockets from "./utils/WebSockets.js"
 // routes
-import indexRouter from "./routes/index.js"
-import userRouter from "./routes/user.js"
-import chatRoomRouter from "./routes/chatRoom.js"
-import deleteRouter from "./routes/delete.js"
+import indexRouter from "./routes/index_routes.js"
+import userRouter from "./routes/user_routes.js"
+import chatRoomRouter from "./routes/chatRoom_routes.js"
+import deleteRouter from "./routes/delete_routes.js"
 
 import { decode } from './middlewares/jwt.js'
 
 // run server with node --experimental-json-modules server.js
 const app = express();
-app.use(bodyparser.json());
+app.use(express.json());
 
 const port = 3000
 app.set("port", port)
@@ -29,12 +29,19 @@ app.use("/users", userRouter);
 app.use("/room", chatRoomRouter); // add decryption here
 app.use("/delete", deleteRouter);
 
+app.use('/test', async (req, res) => {
+    res.status(200).json({message: 'pass!'})
+  })
+
 app.use('*', (req, res) => {
     return res.status(404).json({
         success: false,
         message: 'API endpoint doesnt exist'
     })
 });
+
+
+  
 
 const server = http.createServer(app)
 
@@ -47,3 +54,5 @@ server.listen(port)
 server.on("listening", () => {
     console.log(`Listening on port:: http://localhost:${port}/`)
 });
+
+module.exports = app
