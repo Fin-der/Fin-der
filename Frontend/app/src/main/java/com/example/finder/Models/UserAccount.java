023c9ca0ecf6d6df5af4d215184e5115cf4aad54
nick-ng13 <nicholas_ng@shaw.ca>
2020-11-01 16:33:40 -0800

    adding some functionality for passing profile through intents

--- Frontend/app/src/main/java/com/example/finder/Models/UserAccount.java
+++ Frontend/app/src/main/java/com/example/finder/Models/UserAccount.java
@@ -8,16 +8,23 @@ public class UserAccount implements Serializable {
     private String id;
     private int age;
     private String gender;
-    private ArrayList<UserAccount> matches;
+    private String email;
 
+    private ArrayList<UserAccount> matches;
+    // TODO
+    // change to first name and last name
+    // interest
     //Picture profilePic
     //Location Range
+    // location home
     //Some google token stuff?
 
     public UserAccount(String userName, String id, String gender) {
         this.userName = userName;
         this.id = id;
         this.gender = gender;
+        this.age = age;
+        this.email = email;
     }
 
     public String getId() {
@@ -32,6 +39,10 @@ public class UserAccount implements Serializable {
 
     public int getAge() {
         return age;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
 }
--- Frontend/app/src/main/java/com/example/finder/Views/ProfileView.java
+++ Frontend/app/src/main/java/com/example/finder/Views/ProfileView.java
@@ -5,18 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.finder.Models.UserAccount;
 import com.example.finder.R;
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.w3c.dom.Text;
 
 public class ProfileView extends AppCompatActivity {
     final static String TAG = "ProfileView";
+
+    private TextView fullName;
+    private TextView numMatches;
+    private TextInputLayout firstName;
+    private TextInputLayout lastName;
+    private TextInputLayout age;
+    private TextInputLayout email;
+    private TextInputLayout phoneNumber;
+    private TextInputLayout password;
+
+    private UserAccount user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_view);
+
+        fullName = findViewById(R.id.fullNameText);
+        firstName = findViewById(R.id.first_name_profile);
+        lastName = findViewById(R.id.last_name_profile);
+        numMatches = findViewById(R.id.number_matches);
+
+        this.user = (UserAccount) getIntent().getSerializableExtra("profile");
+
+        fullName.setText(user.getUserName());
+        firstName.getEditText().setText(user.getUserName());
+        lastName.getEditText().setText(user.getUserName());
+
 
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ProfileView.this);
         alertDialogBuilder.setTitle("CONFIRM ACCOUNT DELETION");
