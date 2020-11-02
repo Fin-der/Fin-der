fa7a3f2218f8f18b0dbc5f5156474c9b33044c78
jack5661 <34701860+jack5661@users.noreply.github.com>
2020-11-01 18:53:15 -0800

    Created first test for login screen

---
+++ Frontend/app/src/androidTest/java/com/example/finder/MainActivityBehaviour.java
@@ -0,0 +1,43 @@
+package com.example.finder;
+
+import android.provider.ContactsContract;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.example.finder.Views.CreateAccView;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityBehaviour {
+    @Rule
+    public IntentsTestRule<MainActivity> mLoginActivityActivityTestRule =
+            new IntentsTestRule<>(MainActivity.class);
+    @Test
+    public void pushCreateAccount() {
+        //Intents.init();
+        onView(withId(R.id.create_acc_button)).perform(click());
+        intended(hasComponent(CreateAccView.class.getName()));
+    }
+
+
+}
