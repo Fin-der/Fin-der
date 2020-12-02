67c30ccd3341444f220d4d04e9cf57b08e9fc3af
cody li <licody2000@gmail.com>
2020-12-01 18:21:46 -0800

    codacy

--- Frontend/app/src/main/java/com/example/finder/chat/MyFirebaseMessagingService.java
+++ Frontend/app/src/main/java/com/example/finder/chat/MyFirebaseMessagingService.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Build;
 import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
